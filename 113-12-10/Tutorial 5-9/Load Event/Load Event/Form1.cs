d15e9de04aa6c0f4cc7f5dce38d5dc4b55873636
柳啟東 <qidonglu2004@gmail.com>
2024-12-10 18:02:22 +0800

    20241210

---
+++ 113-12-10/Tutorial 5-9/Load Event/Load Event/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Load_Event
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader inputFile;
+            string countryName;
+            try
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFile.FileName);
+                    countriesListBox.Items.Clear();
+                    while (!inputFile.EndOfStream)
+                    {
+                        countryName = inputFile.ReadLine();
+                        countriesListBox.Items.Add(countryName);
+                    }
+                    inputFile.Close();
+                }
+                else
+                {
+                    MessageBox.Show("未選擇檔案!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
